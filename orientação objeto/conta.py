74fdd97b59916dca3835d44a7d8262fa748c15d5
felipeliralira <felipe.lira.oliveira@escola.pr.gov.br>
2026-04-27 11:46:14 -0300

    Add files via upload

---
+++ orientação objeto/conta.py
@@ -0,0 +1,25 @@
+class Conta:
+    def __init__(self, numero_conta, titular_conta, saldo_conta, limite_conta = 1000):
+        self.numero = numero_conta
+        self.titular = titular_conta
+        self.saldo = saldo_conta
+        self.limite = limite_conta
+
+
+#Crie uma classe que represente um vídeo com os atributos título, duração e views
+class Video:
+    def __init__(self, titulo, duracao, views):
+        self.titulo = titulo
+        self.duracao = duracao
+        self.views = views
+
+video = Video("Baleia Azul", "5:00", 10000)
+
+
+# Como poderia ser criada uma classe que represente o objeto livro = Livro(titulo,autor,data_publicacao)?
+class Livro:
+    def __init__(self, titulo, autor, data_publicacao):
+        self.titulo = titulo
+        self.autor = autor
+        self.data_publicacao = data_publicacao
+livro = Livro("A história do Xamã", "Eduardo Klen", "29/11/2008")
---
+++ orientação objeto/exec.py
@@ -0,0 +1,13 @@
+from conta import Conta
+
+conta = Conta(123456, "João Silva", 1000.0, 2000.0)
+conta1 = Conta(654321, "Maria Souza", 1500.0, 2500.0)
+print(conta)
+print(conta1)
+
+# Suponha que por padrão as contas criadas possuam limite inicial de 1000.0 e apenas contas especiais tem limites diferentes. Como poderiamos declarar os parametros da classe para que o codigo nao fique repetitivo
+
+conta = Conta(123456, "João Silva", 0.0)
+conta1 = Conta(654321, "Maria Souza", 0.0)
+conta2 = Conta(111222, "Carlos Oliveira", 0.0, 2000.0)
+print(conta.limite_conta)
