a477f67bb29a6fbc019d9a0af76c2f54fd85e95f
felipeliralira <felipe.lira.oliveira@escola.pr.gov.br>
2026-05-25 11:29:26 -0300

    Add files via upload

--- orientação objeto/conta.py
+++ orientação objeto/conta.py
@@ -1,28 +1,34 @@
 class Conta:
     def __init__(self, numero_conta, titular_conta, saldo_conta, limite_conta = 1000):
-        self.numero = numero_conta
-        self.titular = titular_conta
-        self.saldo = saldo_conta
-        self.limite = limite_conta
+        self.__numero = numero_conta
+        self.__titular = titular_conta
+        self.__saldo = saldo_conta
+        self.__limite = limite_conta
 
     #Declaração dos métodos (funções)
     def extrato(self):
-        print(f"Saldo atual: {self.saldo} do titular {self.titular}")
+        print(f"Saldo atual: {self.__saldo} do titular {self.__titular}")
 
     def depositar(self, valor):
         if self.valor < 0:
             print("Valor de depósito inválido")
         else:
-            self.saldo += valor
-            print(f"Depósito de {valor} realizado com sucesso. Saldo atual: {self.saldo}")
+            self.__saldo += valor
+            print(f"Depósito de {valor} realizado com sucesso. Saldo atual: {self.__saldo}")
 
     def sacar(self, valor):
-        self.saldo -= valor
-        if self.saldo < valor:
+        self.__saldo -= valor
+        if self.__saldo < valor:
             print("Saldo insuficiente")
         else:
-            print(f"Saque de {valor} realizado com sucesso. Saldo atual: {self.saldo}")
-
+            print(f"Saque de {valor} realizado com sucesso. Saldo atual: {self.__saldo}")
+            
+    def transferir(self, valor, conta_destino):
+        if (self.__saldo < valor) or (valor < 0):
+            print("Valor de transferência inválido ou saldo insuficiente")
+        else:
+            self.sacar(valor)
+            conta_destino.depositar(valor)
 
 #Crie uma classe que represente um vídeo com os atributos título, duração e views
 class Video:
