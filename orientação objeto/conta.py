ab17b90764b258788930c20b2805f50729bf3076
felipeliralira <felipe.lira.oliveira@escola.pr.gov.br>
2026-06-01 11:42:01 -0300

    Add files via upload

--- orientação objeto/conta.py
+++ orientação objeto/conta.py
@@ -1,27 +1,31 @@
 class Conta:
-    def __init__(self, numero_conta, titular_conta, saldo_conta, limite_conta = 1000):
+    def __init__(self, numero_conta, titular_conta, saldo_conta, limite_conta):
         self.__numero = numero_conta
         self.__titular = titular_conta
         self.__saldo = saldo_conta
-        self.__limite = limite_conta
+        self.__limite_especial = limite_conta
 
     #Declaração dos métodos (funções)
     def extrato(self):
         print(f"Saldo atual: {self.__saldo} do titular {self.__titular}")
 
     def depositar(self, valor):
-        if self.valor < 0:
+        if self.valor <= 0:
             print("Valor de depósito inválido")
         else:
             self.__saldo += valor
             print(f"Depósito de {valor} realizado com sucesso. Saldo atual: {self.__saldo}")
 
+    def __saque_permitido(self, valor_saque):
+        valor_disponivel_saque = self.__saldo + self.__limite_especial
+        return valor_saque <= valor_disponivel_saque
+
     def sacar(self, valor):
-        self.__saldo -= valor
-        if self.__saldo < valor:
-            print("Saldo insuficiente")
+        
+        if (self.__saque_permitido(valor)):
+            self.__saldo -= valor
         else:
-            print(f"Saque de {valor} realizado com sucesso. Saldo atual: {self.__saldo}")
+            print(f"O valor {valor} passou do limite.")
             
     def transferir(self, valor, conta_destino):
         if (self.__saldo < valor) or (valor < 0):
@@ -42,10 +46,20 @@ class Conta:
     
     @property
     def limite(self):
-        return self.__limite
+        return self.__limite_especial
+    
     @property
     def numero(self):
         return self.__numero
+    
+    @staticmethod
+    def codigo_banco():
+        return '001'
+    
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237', 'Itaú': '341'}
+    
     
     #Métodos para manipular 
     #os valores das propriedades
--- orientação objeto/exec.py
+++ orientação objeto/exec.py
@@ -2,3 +2,7 @@ from conta import Conta
 
 conta = Conta(123456, "João Silva", 1000.0, 2000.0)
 conta1 = Conta(654321, "Maria Souza", 1500.0, 2500.0)
+
+
+codigos = Conta.codigos_bancos()
+print(codigos['Caixa'])
